a30382390322a7951e192aec9cc10d10cd9632d2
Zachyy-boi <zacharyscottruiz@gmail.com>
2026-08-29 16:29:13 +0800

    refactor(routes): dropped the shelf and about routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import SearchPage from './pages/SearchPage'
 import BookPage from './pages/BookPage'
-import ShelfPage from './pages/ShelfPage'
-import AboutPage from './pages/AboutPage'
 import NotFoundPage from './pages/NotFoundPage'
 import './App.css'
 
@@ -15,8 +13,6 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/book/:workId" element={<BookPage />} />
-        <Route path="/shelf" element={<ShelfPage />} />
-        <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
